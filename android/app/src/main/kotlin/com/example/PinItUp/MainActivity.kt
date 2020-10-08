499b819401c6d6427f5ef7fc45c4ab9c0fd3212c
aiihtbt <imbhaumikjoshiforyou@gmail.com>
2020-10-08 23:09:59 +0530

    Transition from React-Native to Flutter

---
+++ android/app/src/main/kotlin/com/example/PinItUp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.PinItUp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- store.js
+++
@@ -1,9 +0,0 @@
-import { createStore, combineReducers } from 'redux'
-
-const initialState = {};
-
-const store = ()=> {
-    createStore(initialState, combineReducers)
-}
-
-export default store;
